cb39e46c999d39975458099b1f229970835dc17d
marcialc <24596764+marcialc@users.noreply.github.com>
2026-06-24 23:15:09 +0000

    Add Yosemite National Park park

--- src/data/index.ts
+++ src/data/index.ts
@@ -2,10 +2,11 @@ import type { Park } from './types';
 import { yellowstone } from './parks/yellowstone';
 import { glacier } from './parks/glacier';
 import { zion } from './parks/zion';
+import { yosemite } from './parks/yosemite';
 
 // To add a park: create src/data/parks/<slug>.ts exporting a Park object
 // (copy _TEMPLATE.ts), then add it to this array. Nothing else needs editing.
-export const PARKS: Park[] = [yellowstone, glacier, zion];
+export const PARKS: Park[] = [yellowstone, glacier, zion, yosemite];
 
 export const getPark = (slug: string): Park | undefined =>
   PARKS.find((p) => p.slug === slug);
